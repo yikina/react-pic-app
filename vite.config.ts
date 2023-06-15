35a9047a9a9d272312812de74c98b323a3c7229a
wangchulin <13595451343@163.com>
2023-06-15 14:10:56 +0800

    feat: initially add vite typescript config

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import vitePluginImp from 'vite-plugin-imp';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),//react()配置后不需要每页再引入react
+    vitePluginImp({//按需引入 Antd 的样式和组件
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => `antd/es/${name}/style`,
+        }
+      ]
+    })
+  ],
+  resolve: {
+    alias:{
+        '@': path.resolve(__dirname, './src') // 路径别名 
+    },
+    extensions: ['.js', '.ts', '.json'] // 导入时想要省略的扩展名列表
+  },
+  css: {
+      preprocessorOptions: {
+        scss: {//引入项目自定义变量
+          additionalData: `@import "./src/style/_variables.scss";`
+        }
+      }
+    },
+    esbuild: {
+      drop: ["console", "debugger"],
+    },
+  server: {
+    port:3022,//启动端口
+  }
+  
+})
