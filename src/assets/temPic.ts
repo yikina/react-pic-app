4a2fd8d1d85bb99e8ccbd82cb3492b7b5870d7d2
chulin wang <13595451343@163.com>
2023-07-26 18:46:31 +0800

    feat: add me static page

---
+++ src/assets/temPic.ts
@@ -0,0 +1,7 @@
+const userBg = 'https://tuchuangs.com/imgs/2022/09/22/4904f3d712343bc0.jpeg';
+const userFace = 'https://tuchuangs.com/imgs/2023/07/26/4957b2fb5ff903b3.jpg';
+
+export default {
+	userBg,
+	userFace
+};
--- src/views/Me/index.tsx
+++ src/views/Me/index.tsx
@@ -1,5 +1,51 @@
+import Footer from '&/components/Footer';
+import Pic from '&/assets/temPic';
+import { Button, Divider, Empty } from 'antd';
+import './index.scss';
+
 function Me() {
-	return <div>这里是Me</div>;
+	return (
+		<div className="me">
+			<div className="me-bg">
+				<img src={Pic.userBg} alt="" />
+				<i className="iconfont icon-tuichu" />
+			</div>
+
+			<div className="me-header">
+				<img src={Pic.userFace} alt="" />
+				<div className="me-header-name">
+					<h3>yikina</h3>
+					<i className="iconfont icon-wenbenshuru" />
+				</div>
+				<div className="tips">
+					<div>
+						0<h5>粉丝</h5>
+					</div>
+					<div>
+						0<h5>关注</h5>
+					</div>
+					<div>
+						0<h5>徽章</h5>
+					</div>
+				</div>
+				<div className="ip">
+					IP属地:<span>北京</span>
+				</div>
+			</div>
+
+			<div className="me-content">
+				<Button type="text" block>
+					收藏
+				</Button>
+				<Button type="text" block>
+					记录
+				</Button>
+			</div>
+			<Divider />
+			<Empty />
+			<Footer />
+		</div>
+	);
 }
 
 export default Me;
