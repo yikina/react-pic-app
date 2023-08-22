d844cad3f7170b16689ea5cde7a3c822e4f5bb49
chulin wang <13595451343@163.com>
2023-08-22 11:04:10 +0800

    chore: vite.config modified

--- vite.config.ts
+++ vite.config.ts
@@ -4,38 +4,41 @@ import vitePluginImp from 'vite-plugin-imp';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		react(), //react()配置后不需要每页再引入react
-		vitePluginImp({
-			//按需引入 Antd 的样式和组件
-			libList: [
-				{
-					libName: 'antd',
-					style: (name) => `antd/es/${name}/style`
-				}
-			]
-		})
-	],
-	resolve: {
-		alias: {
-			'&': path.resolve(__dirname, './src') // 路径别名
+export default defineConfig(({ command, mode }) => {
+	const isBuild = mode === 'production' && command === 'build';
+
+	return {
+		plugins: [
+			react(), //react()配置后不需要每页再引入react
+			vitePluginImp({
+				//按需引入 Antd 的样式和组件
+				libList: [
+					{
+						libName: 'antd',
+						style: (name) => `antd/es/${name}/style`
+					}
+				]
+			})
+		],
+		resolve: {
+			alias: {
+				'&': path.resolve(__dirname, './src') // 路径别名
+			},
+			extensions: ['.tsx', '.ts', '.js', '.json'] // 导入时想要省略的扩展名列表
 		},
-		extensions: ['.tsx', '.ts', '.js', '.json'] // 导入时想要省略的扩展名列表
-	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				//引入项目自定义变量
-				additionalData: `@import "./src/style/index.scss";`
+		css: {
+			preprocessorOptions: {
+				scss: {
+					//引入项目自定义变量
+					additionalData: `@import "./src/style/index.scss";`
+				}
 			}
+		},
+		esbuild: {
+			drop: isBuild ? ['console'] : []
+		},
+		server: {
+			port: 3022 //启动端口
 		}
-	},
-	esbuild: {
-		//打包时更改
-		// drop: ['console', 'debugger']
-	},
-	server: {
-		port: 3022 //启动端口
-	}
+	};
 });
