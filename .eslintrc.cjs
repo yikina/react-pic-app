c51d7658f54fd3279e5053adacb65b154cbc4fae
wangchulin <13595451343@163.com>
2023-09-13 11:13:30 +0800

    chore: install valtio

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,12 +6,15 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
     'prettier',
+    'plugin:valtio/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'valtio/state-snapshot-rule': 'warn',
+    'valtio/avoid-this-in-proxy' :'warn',
   },
 }
 
